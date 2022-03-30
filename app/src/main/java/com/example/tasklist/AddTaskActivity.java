355c79aaa8dc2b084eede485b3d4e07f84ff5a29
yp6969 <yp6969@gmail.com>
2022-03-30 19:21:50 +0300

    add cancel btn, fix bugs in update tasks

--- app/src/main/java/com/example/tasklist/AddTaskActivity.java
+++ app/src/main/java/com/example/tasklist/AddTaskActivity.java
@@ -18,13 +18,13 @@ import java.util.Calendar;
 
 public class AddTaskActivity extends Activity implements View.OnClickListener {
     private Button addTodoBtn;
+    private Button cancelBtn;
     private EditText subjectEditText;
     private EditText descEditText;
     private RadioGroup progressRadio;
     private RadioGroup urgencyRadio;
-    private DatePickerDialog picker;
-
     private DBManager dbManager;
+
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -41,72 +41,89 @@ public class AddTaskActivity extends Activity implements View.OnClickListener {
         descEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Calendar calender = Calendar.getInstance();
-                int day = calender.get(Calendar.DAY_OF_MONTH);
-                int month = calender.get(Calendar.MONTH);
-                int year = calender.get(Calendar.YEAR);
-                picker = new DatePickerDialog(AddTaskActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-                            @SuppressLint("SetTextI18n")
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                descEditText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-                            }
-                        }, year, month, day);
-                picker.show();
+                setCalender(v);
             }
         });
 
         progressRadio = (RadioGroup) findViewById(R.id.progressRadio);
         urgencyRadio = (RadioGroup) findViewById(R.id.urgencyRadio);
         addTodoBtn = (Button) findViewById(R.id.add_task);
+        cancelBtn = (Button) findViewById(R.id.cancel);
 
         dbManager = new DBManager(this);
         dbManager.open();
+
         addTodoBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.add_task:
-                final String name = subjectEditText.getText().toString();
-                final String desc = descEditText.getText().toString();
-                int status = progressRadio.getCheckedRadioButtonId();
-                RadioButton rb = (RadioButton)findViewById(status);
-                String checked = rb.getText().toString();
-                setDone(checked);
-
-                status = urgencyRadio.getCheckedRadioButtonId();
-                rb = (RadioButton)findViewById(status);
-                String urgencyTask = rb.getText().toString();
-                setUrgency(urgencyTask);
-                if(name.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Enter task title", Toast.LENGTH_LONG).show();
-                }else if(desc.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Enter date to preform", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    dbManager.insert(name, desc, checked, urgencyTask);
-                    Intent main = new Intent(AddTaskActivity.this, MainActivity.class)
-                            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                    startActivity(main);
-                }
+                setAddTask();
+                break;
+            case R.id.cancel:
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
                 break;
         }
     }
+
+    private void setCalender(View v){
+        final Calendar calender = Calendar.getInstance();
+        int day = calender.get(Calendar.DAY_OF_MONTH);
+        int month = calender.get(Calendar.MONTH);
+        int year = calender.get(Calendar.YEAR);
+        DatePickerDialog picker = new DatePickerDialog(AddTaskActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        descEditText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+        picker.show();
+    }
+
+    private void setAddTask(){
+        final String name = subjectEditText.getText().toString();
+        final String desc = descEditText.getText().toString();
+        int status = progressRadio.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton)findViewById(status);
+        String checked = rb.getText().toString();
+        setDone(checked);
+
+        status = urgencyRadio.getCheckedRadioButtonId();
+        rb = (RadioButton)findViewById(status);
+        String urgencyTask = rb.getText().toString();
+        setUrgency(urgencyTask);
+        if(name.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Enter task title", Toast.LENGTH_LONG).show();
+        }else if(desc.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Enter date to preform", Toast.LENGTH_LONG).show();
+        }
+        else{
+            dbManager.insert(name, desc, checked, urgencyTask);
+            Intent main = new Intent(AddTaskActivity.this, MainActivity.class)
+                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            startActivity(main);
+        }
+    }
+
     private void setDone(String s){
         RadioButton rbStart = (RadioButton)findViewById(R.id.in_progress);
         RadioButton rbDone = (RadioButton)findViewById(R.id.tasks_done);
 
-        if(s.equals("Done")){
-            rbStart.setChecked(false);
-            rbDone.setChecked(true);
-        }
-        else{
-            rbStart.setChecked(true);
-            rbDone.setChecked(false);
+        switch (s){
+            case "In progress":
+                rbStart.setChecked(true);
+                rbDone.setChecked(false);
+                break;
+            case "Done":
+                rbStart.setChecked(false);
+                rbDone.setChecked(true);
+                break;
         }
     }
 
@@ -114,13 +131,15 @@ public class AddTaskActivity extends Activity implements View.OnClickListener {
         RadioButton normal = (RadioButton)findViewById(R.id.normal);
         RadioButton urgent = (RadioButton)findViewById(R.id.urgent);
 
-        if(s.equals("Urgent")){
-            normal.setChecked(false);
-            urgent.setChecked(true);
-        }
-        else{
-            normal.setChecked(true);
-            urgent.setChecked(false);
+        switch (s){
+            case "No pressure":
+                normal.setChecked(true);
+                urgent.setChecked(false);
+                break;
+            case "Urgent":
+                normal.setChecked(false);
+                urgent.setChecked(true);
+                break;
         }
     }
 }
--- app/src/main/java/com/example/tasklist/ModifyTaskActivity.java
+++ app/src/main/java/com/example/tasklist/ModifyTaskActivity.java
@@ -45,19 +45,7 @@ public class ModifyTaskActivity extends Activity implements View.OnClickListener
         descText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Calendar cldr = Calendar.getInstance();
-                int day = cldr.get(Calendar.DAY_OF_MONTH);
-                int month = cldr.get(Calendar.MONTH);
-                int year = cldr.get(Calendar.YEAR);
-                // date picker dialog
-                picker = new DatePickerDialog(ModifyTaskActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                descText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-                            }
-                        }, year, month, day);
-                picker.show();
+                setCalender(v);
             }
         });
 
@@ -76,7 +64,7 @@ public class ModifyTaskActivity extends Activity implements View.OnClickListener
         titleText.setText(name);
         descText.setText(desc);
 
-        progressRadio = (RadioGroup) findViewById(R.id.progressRadio_m);
+        progressRadio = (RadioGroup) findViewById(R.id.progressRadio);
         setDone(done);
 
         urgencyRadio = (RadioGroup) findViewById(R.id.urgencyRadio);
@@ -90,25 +78,7 @@ public class ModifyTaskActivity extends Activity implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_update:
-                String title = titleText.getText().toString();
-                String desc = descText.getText().toString();
-                int status = progressRadio.getCheckedRadioButtonId();
-                RadioButton rb = (RadioButton)findViewById(status);
-                String checked = rb.getText().toString();
-
-                status = urgencyRadio.getCheckedRadioButtonId();
-                rb = (RadioButton)findViewById(status);
-                String urgencyTask = rb.getText().toString();
-
-                if(title.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Enter task title", Toast.LENGTH_LONG).show();
-                }else if(desc.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Enter date to preform", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    dbManager.update(_id, title, desc, checked, urgencyTask);
-                    this.returnHome();
-                }
+                updateBtn();
                 break;
 
             case R.id.btn_delete:
@@ -124,17 +94,57 @@ public class ModifyTaskActivity extends Activity implements View.OnClickListener
         startActivity(home_intent);
     }
 
+    private void setCalender(View v){
+        final Calendar calender = Calendar.getInstance();
+        int day = calender.get(Calendar.DAY_OF_MONTH);
+        int month = calender.get(Calendar.MONTH);
+        int year = calender.get(Calendar.YEAR);
+        // date picker dialog
+        picker = new DatePickerDialog(ModifyTaskActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        descText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+        picker.show();
+    }
+
+    private void updateBtn(){
+        String title = titleText.getText().toString();
+        String desc = descText.getText().toString();
+        int status = progressRadio.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton)findViewById(status);
+        String checked = rb.getText().toString();
+
+        status = urgencyRadio.getCheckedRadioButtonId();
+        rb = (RadioButton)findViewById(status);
+        String urgencyTask = rb.getText().toString();
+
+        if(title.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Enter task title", Toast.LENGTH_LONG).show();
+        }else if(desc.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Enter date to preform", Toast.LENGTH_LONG).show();
+        }
+        else{
+            dbManager.update(_id, title, desc, checked, urgencyTask);
+            this.returnHome();
+        }
+    }
+
     private void setDone(String s){
         RadioButton rbStart = (RadioButton)findViewById(R.id.in_progress);
         RadioButton rbDone = (RadioButton)findViewById(R.id.tasks_done);
 
-        if(s.equals("In progress")){
-            rbStart.setChecked(true);
-            rbDone.setChecked(false);
-        }
-        else{
-            rbStart.setChecked(false);
-            rbDone.setChecked(true);
+        switch (s){
+            case "In progress":
+                rbStart.setChecked(true);
+                rbDone.setChecked(false);
+                break;
+            case "Done":
+                rbStart.setChecked(false);
+                rbDone.setChecked(true);
+                break;
         }
     }
 
@@ -142,13 +152,15 @@ public class ModifyTaskActivity extends Activity implements View.OnClickListener
         RadioButton normal = (RadioButton)findViewById(R.id.normal);
         RadioButton urgent = (RadioButton)findViewById(R.id.urgent);
 
-        if(s.equals("Normal")){
-            normal.setChecked(true);
-            urgent.setChecked(false);
-        }
-        else if(s.equals("Urgent")){
-            normal.setChecked(false);
-            urgent.setChecked(true);
+        switch (s){
+            case "No pressure":
+                normal.setChecked(true);
+                urgent.setChecked(false);
+                break;
+            case "Urgent":
+                normal.setChecked(false);
+                urgent.setChecked(true);
+                break;
         }
     }
 }
